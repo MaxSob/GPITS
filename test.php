89921f6cf6f862bfd605420bad200232de138f23
root <mario.campos.soberanis@gmail.com>
2021-02-26 10:24:06 +0000

    Update of the test file

--- test.php
+++ test.php
@@ -1,12 +1,28 @@
-<?php include('StringUtils.php');
+<?php
+include_once("gpits_core.php");
+include_once("XmlDriver.php");
+include_once("MoodleToolsDomainKnowledge.php");
+include_once("mits.php");
+include_once("StringUtils.php");
 
-echo lev('hola', 'olas');
-echo '<br />';
-echo lev('Tomala barbon', 'tomala barbon');
+function getResponse($msg, $user) {
+  $tutor = new MITSTutor();
+  return $tutor->attendRequest($msg, $user);
+}
 
-echo '<br />';
-echo levNormalized(prepareString('La Tarjeta'), prepareString('tarjeta'));
-echo '<br />';
-echo levNormalized(prepareString("tarjeta"), prepareString("La super mega archiduper tarjeta"));
-echo '<br />';
-echo var_dump(compareWords(prepareString("La super mega archiduper tArgeta pocchorrosina y jamelga"), prepareString("tarjeta pochorrosa")));
+function getRecommendation($user) {
+  $tutor = new MITSTutor();
+  return $tutor->decideFedBack($user); 
+}
+
+$msg = "Que es le foro";
+$user = '1';
+$r = ($_REQUEST["r"])?$_REQUEST["r"]:'0';
+
+if($r != '0') {
+  //echo "Recommending <br />";
+  echo getRecommendation($user);
+} else {
+  //echo "Responding to $msg <br />";
+  echo getResponse($msg, $user);
+}
